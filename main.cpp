df060402841ad443ba74069660f6494806535194
Victor <victorribeiro172@gmail.com>
2023-06-02 16:49:26 -0300

    desenho dos dedo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
  * Computacao Grafica
  * Codigo Exemplo: Braco Robotico usando GLUT/OpenGL
- * Autor: Prof. Laurindo de Sousa Britto Neto
+ * Autor: Victor Ribeiro da Silva
  */
 
 // Bibliotecas utilizadas pelo OpenGL
@@ -41,7 +41,7 @@ int main(int argc, char** argv){
 
     // inicia o display usando RGB, double-buffering e z-buffering
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-    glutInitWindowSize(800,600);
+    glutInitWindowSize(1024,768);
     glutInitWindowPosition(100,100);
     glutCreateWindow("Computacao Grafica: Braco Robotico");
 
@@ -74,7 +74,7 @@ void reshape (int w, int h){
 
     // Define a forma do volume de visualizacao para termos
     // uma projecao perspectiva (3D).
-    gluPerspective(60, (float)w/(float)h , 1.0, 9.0); //(angulo, aspecto, ponto_proximo, ponto distante)
+    gluPerspective(70, (float)w/(float)h , 1.0, 9.0); //(angulo, aspecto, ponto_proximo, ponto distante)
     gluLookAt(0.0,0.0,7.0,  // posicao da camera (olho)
               0.0,1.0,0.0,  // direcao da camera (geralmente para centro da cena)
               0.0,1.0,0.0); // sentido ou orientacao da camera (de cabeca para cima)
@@ -90,16 +90,16 @@ void keyboard (unsigned char key, int x, int y){
 		case 'O': ombro = (ombro + 5) % 360; break; // sentido anti-horario
 		case 'c': cotovelo = (cotovelo - 5) % 360; break; // sentido horario
 		case 'C': cotovelo = (cotovelo + 5) % 360; break; // sentido anti-horario
-        case 'm': mao = (mao - 5) % 360; break;
-        case 'M': mao = (mao + 5) % 360; break;
-        case 'y': base = (base - 5) % 360; break;
-        case 'Y': base = (base + 5) % 360; break;
-        case 'i': indicador = (indicador - 5) % 360; break;
-        case 'I': indicador = (indicador + 5) % 360; break;
-        case 'p': polegar = (polegar - 5) % 360; break;
-        case 'P': polegar = (polegar + 5) % 360; break;
-        case 'a': anelar = (anelar - 5) % 360; break;
-        case 'A': anelar = (anelar + 5) % 360; break;
+        case 'm': mao = (mao - 5) % 360; break; // sentido horario
+        case 'M': mao = (mao + 5) % 360; break; // sentido anti-horario
+        case 'y': base = (base - 5) % 360; break; // sentido horario
+        case 'Y': base = (base + 5) % 360; break; // sentido anti-horario
+        case 'i': indicador = (indicador - 5) % 360; break; // sentido horario
+        case 'I': indicador = (indicador + 5) % 360; break; // sentido anti-horario
+        case 'p': polegar = (polegar - 5) % 360; break; // sentido horario
+        case 'P': polegar = (polegar + 5) % 360; break; // sentido anti-horario
+        case 'a': anelar = (anelar - 5) % 360; break; // sentido horario
+        case 'A': anelar = (anelar + 5) % 360; break; // sentido anti-horario
 		case ESC: exit(EXIT_SUCCESS); break; // sai do programa
 	}
     glutPostRedisplay();
@@ -110,131 +110,132 @@ void display(void){
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Limpa o Buffer de Cores
     glLoadIdentity();
     
-glPushMatrix();
-	glRotatef ((GLfloat) base, 0.0, 1.0, 0.0);
-    //Base
-    glPushMatrix();
-        glTranslatef(0.0, -2.0, 0.0);
-        glRotatef(-90, 1, 0, 0);
-        glColor3f(1,0.647,0.0);
-      glutSolidCone(1.0, 1.0, 30, 30); // base, altura, fatias, pilhas
-    glPopMatrix();
-    
-    //Ombro
-    glPushMatrix();
-        glTranslatef(0.0, -1.0, 0.0);
-        glColor3f(0.3,0.3,0.3);
-        glutSolidSphere(0.5, 30, 30); // raio, fatias, pilhas
-    glPopMatrix();
-
-    //Movimento do braco
-    glPushMatrix();
-        // origem posicionada no ombro
-        glTranslatef (0.0, -1.0, 0.0);
-        glRotatef ((GLfloat) ombro, 0.0, 0.0, 1.0);
-        // origem posicionada no centro do braco
-        glTranslatef (0.0, 1.0, 0.0);
-        
-        
-        // Braco
-        glPushMatrix();
-            glRotatef(90, 0, 0, 1);
-            glScalef (2.5, 0.5, 0.5);
-            glColor3f(1,0.647,0.0);
-            glutSolidCube (1.0); // Tamanho
-        glPopMatrix();
-
-        //Cotovelo
-       glPushMatrix();
-	   	   glTranslatef(0.0, 1.0, 0.0);
-           glColor3f(0.3,0.3,0.3);
-		   glutSolidSphere(0.42, 30, 30); // raio, fatias, pilhas
-       glPopMatrix();
-
-        //Movimento do antebraco
-        glPushMatrix();
-            //origem posicionada no cotovelo
-            glTranslatef (0.0, 1.0, 0.0);
-            glutWireSphere(0.4, 30, 30); // raio, fatias, pilhas
-            glRotatef ((GLfloat) cotovelo, 0.0, 0.0, 1.0);
-            glTranslatef (0.0, 1.0, 0.0);
-            
-            
-            // Antebraco
-            glPushMatrix();
-                glRotatef(90, 0, 0, 1);
-                glScalef (2.0, 0.4, 0.4);
-                glColor3f(1,0.647,0.0);
-                glutSolidCube (0.9); // Tamanho
-            glPopMatrix();
-            
-            
-			//Movimento da mão
-			glPushMatrix();
-	            //origem posicionada no abtebraço
-	            glTranslatef (0.0, 1.0, 0.0);
-	            glColor3f(0.1,0.3,0.3);
-	            glutSolidSphere(0.3, 30, 30); // raio, fatias, pilhas
-	            glRotatef ((GLfloat) mao, 0.0, 0.0, 1.0);
-	            glTranslatef (0.0, 1.0, 0.0);
+	glPushMatrix();
+		glRotatef ((GLfloat) base, 0.0, 1.0, 0.0);
+	    //Base
+	    glPushMatrix();
+	        glTranslatef(0.0, -2.0, 0.0);
+	        glRotatef(-90, 1, 0, 0);
+	        glColor3f(1,0.647,0.0);
+	      glutSolidCone(1.0, 1.0, 30, 30); // base, altura, fatias, pilhas
+	    glPopMatrix();
+	    
+	    //Ombro
+	    glPushMatrix();
+	        glTranslatef(0.0, -1.0, 0.0);
+	        glColor3f(0.3,0.3,0.3);
+	        glutSolidSphere(0.5, 30, 30); // raio, fatias, pilhas
+	    glPopMatrix();
+	
+	    //Movimento do braco
+	    glPushMatrix();
+	        // origem posicionada no ombro
+	        glTranslatef (0.0, -1.0, 0.0);
+	        glRotatef ((GLfloat) ombro, 0.0, 0.0, 1.0);
+	        // origem posicionada no centro do braco
+	        glTranslatef (0.0, 1.0, 0.0);
+	        
+	        
+	        // Braco
+	        glPushMatrix();
+	            glRotatef(90, 0, 0, 1);
+	            glScalef (2.5, 0.5, 0.5);
+	            glColor3f(1,0.647,0.0);
+	            glutSolidCube (1.0); // Tamanho
+	        glPopMatrix();
+	
+	        //Cotovelo
+	       glPushMatrix();
+		   	   glTranslatef(0.0, 1.0, 0.0);
+	           glColor3f(0.3,0.3,0.3);
+			   glutSolidSphere(0.42, 30, 30); // raio, fatias, pilhas
+	       glPopMatrix();
+	
+	       //Movimento do antebraco
+   	   	   glPushMatrix();
+	            //origem posicionada no cotovelo
+				glTranslatef (0.0, 1.0, 0.0);
+				glutSolidSphere(0.4, 30, 30); // raio, fatias, pilhas
+				glRotatef ((GLfloat) cotovelo, 0.0, 0.0, 1.0);
+				glTranslatef (0.0, 1.0, 0.0);
 	            
-				//Mão
+	            
+	            // Antebraco
 	            glPushMatrix();
-	            	glRotatef(90, 0, 0, 1);
-	            	glScalef (2.0, 0.4, 0.4);
-	            	glColor3f(1,0.647,0.0);
-	            	glutSolidCube (0.8); // Tamanho
-				glPopMatrix();
-				
-				//Movimento dos dedos
+	                glRotatef(90, 0, 0, 1);
+	                glScalef (2.0, 0.4, 0.4);
+	                glColor3f(1,0.647,0.0);
+	                glutSolidCube (0.9); // Tamanho
+	            glPopMatrix();
+	            
+	            
+				//Movimento da mão
 				glPushMatrix();
-					//Origem posicionada na mão
-					glTranslatef(0.0, 1.0, 0.0);
-					glColor3f(0.3,0.3,0.3);
-					glutSolidSphere(0.2, 30, 30);
-					glRotatef ((GLfloat) anelar, 0.0, 0.0, 1.0);
-					glTranslatef(0.0, 1.0, 0.0);
-					
-					//Anelar
-					glPushMatrix();
-	            		glRotatef(90, 0, 0, 1);
-	            		glScalef (2.0, 0.4, 0.4);
-	            		glColor3f(1,0.647,0.0);
-	            		glutSolidCube (0.5); // Tamanho
-					glPopMatrix();
-				 
-				 
-					glRotatef ((GLfloat) polegar, 0.0, 0.0, 1.0);
-					
-					
-					//Polegar
-					glPushMatrix();
-	            		glRotatef(90, 0, 0, 1);
-	            		glScalef (2.0, 0.4, 0.4);
-	            		glColor3f(0,0.647,0.0);
-	            		glutSolidCube (0.2); // Tamanho
+		            //origem posicionada no abtebraço
+		            glTranslatef (0.0, 1.0, 0.0);
+		            glColor3f(0.3,0.3,0.3);
+		            glutSolidSphere(0.3, 30, 30); // raio, fatias, pilhas
+		            glRotatef ((GLfloat) mao, 0.0, 0.0, 1.0);
+		            glTranslatef (0.0, 1.0, 0.0);
+		            
+					//mao-braco
+		            glPushMatrix();
+		            	glRotatef(90, 0, 0, 1);
+		            	glScalef (2.0, 0.4, 0.4);
+		            	glColor3f(1,0.647,0.0);
+		            	glutSolidCube (0.9); // Tamanho
 					glPopMatrix();
 					
-					
-					glRotatef ((GLfloat) indicador, 0.0, 0.0, 1.0);
-					
-					
-					//Indicador
+					//Movimento dos dedos
 					glPushMatrix();
-	            		glRotatef(90, 0, 0, 1);
-	            		glScalef (3.0, 0.4, 0.4);
-	            		glColor3f(0,0.647,1.0);
-	            		glutSolidCube (0.2); // Tamanho
+						//Origem posicionada na mão
+						glPushMatrix();
+							glTranslatef(0.0, 1.0, 0.0);
+							glColor3f(0.3,0.3,0.3);
+							glutSolidCube(0.7);
+							glTranslatef(0.0, 1.0, 0.0);
+						 glPopMatrix();
+						 
+						 //Anelar
+						glPushMatrix();
+							glTranslatef(-0.2, 1.0, 0.2);
+							glRotatef ((GLfloat) anelar, 0.0, 0.0, 1.0);
+							glTranslatef(0.0, 0.6, 0.0);
+		            		glRotatef(90, 0, 0, 1);
+		            		glScalef (2.0, 0.4, 0.4);
+		            		glColor3f(1,0.647,0.0);
+		            		glutSolidCube (0.5); // Tamanho
+						glPopMatrix();
+						 
+						//Polegar
+						glPushMatrix();
+							glTranslatef(0.2, 1.0, 0.0);
+							glRotatef ((GLfloat) polegar, 0.0, 0.0, 1.0);
+							glTranslatef(0.0, 0.6, 0.0);
+		            		glRotatef(90, 0, 0, 1);
+		            		glScalef (2.0, 0.4, 0.4);
+		            		glColor3f(1,0.647,0.0);
+		            		glutSolidCube (0.5); // Tamanho
+						glPopMatrix();
+			
+						
+						//Indicador
+						glPushMatrix();
+							glTranslatef(-0.2, 1.0, -0.2);
+							glRotatef ((GLfloat) indicador, 0.0, 0.0, 1.0);
+							glTranslatef(0.0, 0.6, 0.0);
+		            		glRotatef(90, 0, 0, 1);
+		            		glScalef (2.0, 0.4, 0.4);
+		            		glColor3f(1,0.647,0.0);
+		            		glutSolidCube (0.5); // Tamanho
+						glPopMatrix();
+	
+						
 					glPopMatrix();
-
-					
-			glPopMatrix();
+				glPopMatrix();
+  			glPopMatrix();
 		glPopMatrix();
-
-    // origem volta para o sistema de coordenadas original
-    glPopMatrix();
-glPopMatrix();
-
+	glPopMatrix();
     // Troca os buffers, mostrando o que acabou de ser desenhado
     glutSwapBuffers();
 }
